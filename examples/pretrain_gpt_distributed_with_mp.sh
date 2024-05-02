f08fd9dfdfd12c2f86146e8a60746f842ba4f6f5
Junyi Zhang <junyizhang@hust.edu.cn>
2024-05-02 04:33:57 +0000

    Enable moe training in docker

--- examples/pretrain_gpt_distributed_with_mp.sh
+++ examples/pretrain_gpt_distributed_with_mp.sh
@@ -2,38 +2,47 @@
 
 # Runs the "345M" parameter model
 
-GPUS_PER_NODE=8
+GPUS_PER_NODE=2
 # Change for multinode config
 MASTER_ADDR=localhost
-MASTER_PORT=6000
+MASTER_PORT=29504
 NNODES=1
 NODE_RANK=0
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 
-DATA_PATH=<Specify path and file prefix>_text_document
-CHECKPOINT_PATH=<Specify path>
+# DATA_PATH=<Specify path and file prefix>_text_document
+# CHECKPOINT_PATH=<Specify path>
+
+DATA_PREFIX=$DATA_PREFIX
+DATA_PATH=$DATA_PREFIX/datasets/meg-gpt2_text_document
+CHECKPOINT_PATH=$DATA_PREFIX/checkpoints/gpt2_fastmoe
+VOCAB_FILE=$DATA_PREFIX/gpt2-vocab.json
+MERGE_FILE=$DATA_PREFIX/gpt2-merges.txt
 
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
 python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        pretrain_gpt.py \
-       --tensor-model-parallel-size 2 \
-       --pipeline-model-parallel-size 2 \
-       --sequence-parallel \
+       --tensor-model-parallel-size 1 \
+       --pipeline-model-parallel-size 1 \
        --num-layers 24 \
        --hidden-size 1024 \
-       --num-attention-heads 16 \
+       --num-attention-heads 8 \
        --micro-batch-size 4 \
-       --global-batch-size 16 \
+       --global-batch-size 8 \
        --seq-length 1024 \
        --max-position-embeddings 1024 \
-       --train-iters 500000 \
+       --train-iters 1000 \
        --lr-decay-iters 320000 \
        --save $CHECKPOINT_PATH \
        --load $CHECKPOINT_PATH \
        --data-path $DATA_PATH \
-       --vocab-file gpt2-vocab.json \
-       --merge-file gpt2-merges.txt \
+       --vocab-file $VOCAB_FILE \
+       --merge-file $MERGE_FILE \
+       --fmoefy \
+       --num-experts 2 \
+       --fmoe-num-experts 2 \
+       --balance-strategy gshard \
        --data-impl mmap \
        --split 949,50,1 \
        --distributed-backend nccl \
@@ -43,9 +52,8 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --weight-decay 1e-2 \
        --clip-grad 1.0 \
        --lr-warmup-fraction .01 \
-       --activations-checkpoint-method uniform \
-       --log-interval 100 \
+       --no-contiguous-buffers-in-local-ddp \
+       --log-interval 1 \
        --save-interval 10000 \
        --eval-interval 1000 \
-       --eval-iters 10 \
-       --fp16
+       --eval-iters 10
