2546072fd21752d5998f22d249ca6f514500f36b
Junyi Zhang <junyizhang@hust.edu.cn>
2024-05-21 10:25:17 +0000

    Docker training scripts

--- examples/pretrain_bert_distributed_with_mp.sh
+++ examples/pretrain_bert_distributed_with_mp.sh
@@ -1,35 +1,52 @@
 #!/bin/bash
 
-GPUS_PER_NODE=8
+GPUS_PER_NODE=2
 # Change for multinode config
 MASTER_ADDR=localhost
-MASTER_PORT=6000
+MASTER_PORT=29505
 NNODES=1
 NODE_RANK=0
-WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+WORLD_SIZE=$(($GPUS_PER_NODE * $NNODES))
 
-DATA_PATH=<Specify path and file prefix>_text_sentence
-VOCAB_FILE=<Specify path to vocab.txt>
-CHECKPOINT_PATH=<Specify path>
+CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+
+# EP_SIZE=2
+EP_SIZE=$1
+
+METHOD=$2
+
+# DATA_PATH=<Specify path and file prefix>_text_sentence
+# VOCAB_FILE=<Specify path to vocab.txt>
+# CHECKPOINT_PATH=<Specify path>
+
+BERT_PREFIX=$BERT_PREFIX
+DATA_PATH=$BERT_PREFIX/datasets/meg-bert_text_sentence
+CHECKPOINT_PATH=$BERT_PREFIX/checkpoints/bert_${METHOD}_ep_${EP_SIZE}_${CURRENT_TIME}
+LOG_PATH=$BERT_PREFIX/logs
+VOCAB_FILE=$BERT_PREFIX/bert-large-uncased-vocab.txt
 
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
 python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        pretrain_bert.py \
-       --tensor-model-parallel-size 2 \
-       --pipeline-model-parallel-size 2 \
+       --tensor-model-parallel-size 1 \
+       --pipeline-model-parallel-size 1 \
        --num-layers 24 \
        --hidden-size 1024 \
-       --num-attention-heads 16 \
-       --micro-batch-size 2 \
-       --global-batch-size 16 \
+       --num-attention-heads 8 \
+       --micro-batch-size 4 \
+       --global-batch-size 8 \
        --seq-length 512 \
        --max-position-embeddings 512 \
-       --train-iters 1000000 \
+       --train-iters 1000 \
        --save $CHECKPOINT_PATH \
        --load $CHECKPOINT_PATH \
        --data-path $DATA_PATH \
        --vocab-file $VOCAB_FILE \
+       --fmoefy \
+       --num-experts $EP_SIZE \
+       --fmoe-num-experts $EP_SIZE \
+       --balance-strategy gshard \
        --data-impl mmap \
        --split 949,50,1 \
        --distributed-backend nccl \
@@ -40,8 +57,9 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --weight-decay 1e-2 \
        --clip-grad 1.0 \
        --lr-warmup-fraction .01 \
-       --log-interval 100 \
+       --no-contiguous-buffers-in-local-ddp \
+       --log-interval 1 \
        --save-interval 10000 \
        --eval-interval 1000 \
        --eval-iters 10 \
-       --fp16
+       2>&1 | tee $LOG_PATH/bert_${METHOD}_ep_${EP_SIZE}.log
--- examples/pretrain_gpt_distributed_with_mp.sh
+++ examples/pretrain_gpt_distributed_with_mp.sh
@@ -8,14 +8,21 @@ MASTER_ADDR=localhost
 MASTER_PORT=29504
 NNODES=1
 NODE_RANK=0
-WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+WORLD_SIZE=$(($GPUS_PER_NODE * $NNODES))
+
+CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+
+# EP_SIZE=4
+EP_SIZE=$1
+
+METHOD=$2
 
 # DATA_PATH=<Specify path and file prefix>_text_document
 # CHECKPOINT_PATH=<Specify path>
 
 DATA_PREFIX=$DATA_PREFIX
 DATA_PATH=$DATA_PREFIX/datasets/meg-gpt2_text_document
-CHECKPOINT_PATH=$DATA_PREFIX/checkpoints/gpt2_fastmoe
+CHECKPOINT_PATH=$DATA_PREFIX/checkpoints/gpt2_${METHOD}_ep_${EP_SIZE}_${CURRENT_TIME}
 LOG_PATH=$DATA_PREFIX/logs
 VOCAB_FILE=$DATA_PREFIX/gpt2-vocab.json
 MERGE_FILE=$DATA_PREFIX/gpt2-merges.txt
@@ -26,7 +33,7 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        pretrain_gpt.py \
        --tensor-model-parallel-size 1 \
        --pipeline-model-parallel-size 1 \
-       --num-layers 24 \
+       --num-layers 12 \
        --hidden-size 1024 \
        --num-attention-heads 8 \
        --micro-batch-size 4 \
@@ -41,8 +48,8 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --vocab-file $VOCAB_FILE \
        --merge-file $MERGE_FILE \
        --fmoefy \
-       --num-experts 2 \
-       --fmoe-num-experts 2 \
+       --num-experts $EP_SIZE \
+       --fmoe-num-experts $EP_SIZE \
        --balance-strategy gshard \
        --data-impl mmap \
        --split 949,50,1 \
@@ -58,4 +65,4 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --save-interval 10000 \
        --eval-interval 1000 \
        --eval-iters 10 \
-       2>&1 | tee $CHECKPOINT_PATH/megatron_log.txt
+       2>&1 | tee $LOG_PATH/gpt2_${METHOD}_ep_${EP_SIZE}.log
